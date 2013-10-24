76c781896de4ee33798b997518c37faa47b8fb23
KillerDesigner <git@killerdesigner.com>
2013-10-24 09:18:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winoes::Application.config.secret_token = '065e786b02de4676addf9dbcd9ddb5e0c529fdfc6d59a59b7dfed14f2263b5f88e596bec7df53da4848ff9a895aab2b2010d4ae7101916771848093d9e03fb93'
